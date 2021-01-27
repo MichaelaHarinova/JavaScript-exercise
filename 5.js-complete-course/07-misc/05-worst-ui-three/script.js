aa418e3fc13982754a825f24496ead2cc80d0fbc
Micha <michaelaharin@gmail.com>
2021-01-27 15:23:01 +0100

    finished last exercise 7.5

--- 5.js-complete-course/07-misc/05-worst-ui-three/script.js
+++ 5.js-complete-course/07-misc/05-worst-ui-three/script.js
@@ -9,8 +9,40 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+//randomizer with data-max and data-min values
+// The maximum is inclusive and the minimum is inclusive
+function random (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 
-    // your code here
+}
+
+(function () {
+
+
+    document.querySelectorAll("button").forEach(button => button.addEventListener("click", event => {
+        //get the button ID and remove the "fix-" part -> to match it with ID of input
+        let adjustedButtonID = event.target.getAttribute("id").replace("fix-", "");
+        //getting values (max,min) from the elements
+        let minValue =  document.getElementById(adjustedButtonID).getAttribute("data-min");
+        let maxValue =  document.getElementById(adjustedButtonID).getAttribute("data-max");
+
+        //display value of the random number in the input field with a leading 0
+        document.getElementById(adjustedButtonID).value = random(minValue,maxValue).toString().padStart(2, '0');
+
+
+
+
+
+      document.getElementById("target").innerText = "+";
+        document.querySelectorAll("input").forEach(input=>
+            document.getElementById("target").innerText =
+                document.getElementById("target").innerText + input.value.toString());
+
+    }));
 
 })();
+
+
+
